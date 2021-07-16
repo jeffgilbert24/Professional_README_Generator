26952fc5349f5b63892cfd8d2fac222ed0c70d6c
IBBear <jgilbert868@gmail.com>
2021-07-16 16:27:09 -0700

    created readme file

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = requirer('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'discription',
+        message: 'describe your project'
+    },
+    ])
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+fs.writeFile("README.md", process.argv[2], (error) =>{
+    if(error){
+        return console.log(error)
+    }else{
+        console.log("file saved")
+    }
+});
+ 
 // TODO: Create a function to initialize app
 function init() {}
 
