4c9269bd4fe97a33d3325a6369a858478d9ee5e6
IBBear <jgilbert868@gmail.com>
2021-07-18 14:00:54 -0700

    I have an error I can not fix

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ const generateReadMe = (answers) =>
 // TODO: Create an array of questions for user input
 inquirer
     .prompt([
-    {
+  //  {
     
-        type: 'input',
-        name: 'Title',
-        message: 'What is the title of your project?'
-    },
+    //    type: 'input',
+      //  name: 'Title',
+    //    message: 'What is the title of your project?'
+   // },
     {
         type: 'input',
         name: 'Discription',
@@ -70,20 +70,8 @@ inquirer
         type: 'input',
         name: 'license',
         message: 'choose a license form the list.'
-    }
-]);
-const tableOfContents = [
-    'title',
-    'Description',
-    'License'
-]
-tableOfContents.forEach((item, index) =>{
-    questions[index].name = item
-})
-    
-
-
-
+    },
+])
 // TODO: Create a function to initialize app
 .then((answers) =>{
   const ReadMePageContent = generateReadMe(answers);
@@ -93,13 +81,19 @@ tableOfContents.forEach((item, index) =>{
 })
 
 
+const tableOfContents = [
+  'title',
+  'Description',
+  'License'
+]
+tableOfContents.forEach((item, index) =>{
+  questions[index].name = item
+})
+  
 
 
 
 
 
 
-
-// Function call to initialize app
-init();
 
